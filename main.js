eda127071fb5bcb89b765f3a88db8ee84f8911cb
suratshavkatov1002 <shavkatovsurat321@gmail.com>
2022-07-05 10:36:47 +0500

    vazifa-5

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var alishPuli = prompt("Pulingizni kiriting:");
+
+var boribKelish = 500 * 9433.34;
+
+var mexmonxonaTolovi = 250 * 9433.34;
+
+var muzeyKongilochar = 10354.03 * 120;
+
+var jamiSum = boribKelish + mexmonxonaTolovi + muzeyKongilochar;
+
+if(alishPuli >= jamiSum) {
+    console.log("Oq yo’l, Alisher!");
+}else{
+    console.log("Alisher, ozgina sabr qilish kerak bo’lar ekan."); 
+}
+
+
+
